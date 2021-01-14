fe69deb88af15d18b65f97bf14748a4f58abd62a
ruhtra <ruhtra.mar@gmail.com>
2021-01-14 16:48:08 +0100

    git merge

--- src/Components/CompoMe/Component_Core_Component_Control_handle_function.cpp
+++ src/Components/CompoMe/Component_Core_Component_Control_handle_function.cpp
@@ -1,5 +1,6 @@
 #include "Components/CompoMe/Component_Core.hpp"
 #include "Components/CompoMe/Component_Core_Component_Control_handle.hpp"
+#include "Errors/CompoMe/Component_Core_ns/Wrong_state_transition.hpp"
 #include <iostream>
 
 namespace CompoMe {
@@ -7,9 +8,29 @@ namespace CompoMe {
 void Component_Core_Component_Control_handle::step() {
   auto v_state = this->get_c().get_state();
 
-  if (is(v_state, Component_state::STARTED)) {
+  if (!is(v_state, Component_state::STARTED)) {
     C_ERROR("Wrong state translation",
             "to do a step you need to start component first");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(v_state);
+      s.set_from(v_state);
+      s.set_msg("This component is not STARTED");
+      s.real();
+    }
+    return;
+  }
+
+  if (is(v_state, Component_state::DEAD)) {
+    C_ERROR("Wrong state translation",
+            "to do a step your component need to stop and restart");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(v_state);
+      s.set_from(v_state);
+      s.set_msg("This component is DEAD");
+      s.real();
+    }
     return;
   }
 
@@ -21,21 +42,45 @@ void Component_Core_Component_Control_handle::start() {
 
   auto v_state = this->get_c().get_state();
 
-  if (is(v_state, Component_state::CONFIGURED)) {
+  if (!is(v_state, Component_state::CONFIGURED)) {
     C_ERROR("Wrong state translation",
             "to do a start you need to configure component first");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(Component_state::STARTED);
+      s.set_from(v_state);
+      s.set_msg("This component is not CONFIGURED");
+      s.real();
+    }
+
     return;
   }
 
-  if (is(v_state, Component_state::CONNECTED)) {
+  if (!is(v_state, Component_state::CONNECTED)) {
     C_ERROR("Wrong state translation",
             "to do a start you need to connect component first");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(Component_state::STARTED);
+      s.set_from(v_state);
+      s.set_msg("This component is not CONNECTED");
+      s.real();
+    }
+
     return;
   }
 
   if (is(v_state, Component_state::STARTED)) {
     C_ERROR("Wrong state translation",
             "You cannot call a start on a component already started");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(Component_state::STARTED);
+      s.set_from(v_state);
+      s.set_msg("This component is already STARTED");
+      s.real();
+    }
+
     return;
   }
 
@@ -47,9 +92,17 @@ void Component_Core_Component_Control_handle::start() {
 void Component_Core_Component_Control_handle::stop() {
   auto v_state = this->get_c().get_state();
 
-  if (is(v_state, Component_state::STARTED)) {
+  if (!is(v_state, Component_state::STARTED)) {
     C_ERROR("Wrong state translation",
             "to do a stop you need to start component first");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(v_state);
+      s.set_from(v_state);
+      s.set_msg("This component is already STARTED");
+      s.real();
+    }
+
     return;
   }
 
@@ -70,11 +123,16 @@ void Component_Core_Component_Control_handle::uninit() {
 void Component_Core_Component_Control_handle::connect() {
   auto v_state = this->get_c().get_state();
 
-  // if (CompoMe::is(v_state, Component_state::INITIALIZED) == true) {
-  //   C_ERROR("Wrong state translation",
-  //           "to do a configured you need to initialized component first");
-  //   return;
-  // }
+  if (!CompoMe::is(v_state, Component_state::INITIALIZED) == true) {
+    C_ERROR("Wrong state translation",
+            "to do a configured you need to initialized component first");
+    Component_Core_ns::Wrong_state_transition s;
+    s.set_to(Component_state::CONNECTED);
+    s.set_from(v_state);
+    s.set_msg("This component is not initialized");
+    s.real();
+    return;
+  }
 
   this->get_c().connection();
   this->get_c().set_state(v_state | Component_state::CONNECTED);
@@ -83,11 +141,18 @@ void Component_Core_Component_Control_handle::connect() {
 void Component_Core_Component_Control_handle::configure() {
 
   auto v_state = this->get_c().get_state();
-  // if (is(v_state, Component_state::INITIALIZED)) {
-  //   C_ERROR("Wrong state translation",
-  //           "to do a configured you need to initialized component first");
-  //   return;
-  // }
+  if (!is(v_state, Component_state::INITIALIZED)) {
+    C_ERROR("Wrong state translation",
+            "to do a configured you need to initialized component first");
+    {
+      Component_Core_ns::Wrong_state_transition s;
+      s.set_to(Component_state::CONFIGURED);
+      s.set_from(v_state);
+      s.set_msg("This component is not initialized");
+      s.real();
+    }
+    return;
+  }
 
   this->get_c().configuration();
   this->get_c().set_state(v_state | Component_state::CONFIGURED);
