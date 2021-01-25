948e258c860d60ff52527fe66b19521ee6ac66e2
ruhtra <ruhtra.mar@gmail.com>
2021-01-25 19:21:18 +0100

    mise en place de vec dbus

--- inc/Types/Vec.hpp
+++ inc/Types/Vec.hpp
@@ -11,10 +11,39 @@ namespace std {
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const Vec<T> &) {
+  os << "[";
+  bool f = false;
+  for (const T &i_t : t) {
+    if (f) {
+      os << ",";
+    } else {
+      f = true;
+    }
+    os << i_t;
+  }
+  os << "]";
   return os;
 }
 
 template <typename T> std::istream &operator>>(std::istream &is, Vec<T> &) {
+  t = Vec<T>();
+
+  if (is.peek() != '[') {
+    return is;
+  }
+  is.get();
+
+  if (is.peek() == ']') {
+    is.get();
+    return is;
+  }
+
+  do {
+    T a;
+    is >> a;
+    t.push_back(a);
+  } while (is.get() != ']');
+
   return is;
 }
 } // namespace std
