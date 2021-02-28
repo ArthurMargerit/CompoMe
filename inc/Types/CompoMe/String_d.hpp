59cff9f86a7a2e82c9d2e812a99eae8d34481d9b
tiregram <ruhtra.mar@gmail.com>
2021-03-01 00:24:17 +0100

    add string const

--- inc/Types/CompoMe/String_d.hpp
+++ inc/Types/CompoMe/String_d.hpp
@@ -7,16 +7,16 @@ namespace CompoMe {
 class String_d {
 public:
   String_d() : str("") {}
-  String_d(std::string &p_str) : str(p_str) {}
+  String_d(const std::string &p_str) : str(p_str) {}
   String_d(const char *p_str) : str(p_str) {}
 
-  operator const std::string&() const { return this->str; }
+  operator const std::string &() const { return this->str; }
   std::string str;
 };
 
 bool operator==(const CompoMe::String_d &a, const CompoMe::String_d &b);
 std::ostream &operator<<(std::ostream &os, const CompoMe::String_d &c);
-bool operator<( const CompoMe::String_d &b, const CompoMe::String_d & a);
+bool operator<(const CompoMe::String_d &b, const CompoMe::String_d &a);
 std::istream &operator>>(std::istream &is, CompoMe::String_d &c);
 } // namespace CompoMe
 
