88d759495a070d32cc04e8a17d946873078e2685
ArthurMargerit <arthur.margerit.ext@exxact-robotics.com>
2021-06-17 18:06:59 +0200

    update Function and return stream

--- inc/Interfaces/Function_stream_send_func.hpp
+++ inc/Interfaces/Function_stream_send_func.hpp
@@ -2,8 +2,8 @@
 
 #include "Interfaces/Function_stream_send.hpp"
 #include "Types/CompoMe/String.hpp"
-#include <sstream>
 #include <functional>
+#include <sstream>
 
 namespace CompoMe {
 class Function_stream_send_func : public CompoMe::Function_stream_send {
@@ -13,15 +13,24 @@ private:
   std::stringstream ss;
 
 public:
-
   const f_bs get_func_send() const { return func_send; }
   void set_func_send(const f_bs func_send) { this->func_send = func_send; }
 
   Function_stream_send_func();
+  Function_stream_send_func(const CompoMe::Function_stream_send_func&){}
   virtual ~Function_stream_send_func();
 
   std::ostream &get_so() override;
   void start() override;
   void send() override;
+
+  CompoMe::Function_stream_send_func &
+  operator=(const CompoMe::Function_stream_send_func &t) {
+    if (this == &t) {
+      return *this;
+    }
+
+    return *this;
+  }
 };
 } // namespace CompoMe
--- inc/Interfaces/Return_stream_recv_func.hpp
+++ inc/Interfaces/Return_stream_recv_func.hpp
@@ -2,8 +2,8 @@
 
 #include "Interfaces/Return_stream_recv.hpp"
 #include "Types/CompoMe/String.hpp"
-#include <sstream>
 #include <functional>
+#include <sstream>
 
 namespace CompoMe {
 class Return_stream_recv_func : public Return_stream_recv {
@@ -17,11 +17,21 @@ public:
   void set_func_recv(const f_bs func_recv) { this->func_recv = func_recv; }
 
   Return_stream_recv_func();
+  Return_stream_recv_func(const CompoMe::Return_stream_recv_func&){}
   virtual ~Return_stream_recv_func();
 
   std::istream &get_si() override;
 
   void pull() override;
   void end() override;
+
+  CompoMe::Return_stream_recv_func&
+  operator=(const CompoMe::Return_stream_recv_func &t) {
+    if (this == &t) {
+      return *this;
+    }
+
+    return *this;
+  }
 };
 } // namespace CompoMe
